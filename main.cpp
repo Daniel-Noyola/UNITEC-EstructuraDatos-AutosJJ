568455da84ff523be856587633cb02016de5f3fc
Daniel Noyola <pepenoyola13@gmail.com>
2026-01-28 12:40:34 -0600

    v1

---
+++ main.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <conio.h>
+
+using namespace std;
+
+const int ANCHO_CAJA = 80;
+// ---- Tipografía ----
+const string TEXTO_BOLD = "\033[1m";
+const string TEXTO_ITALIC = "\033[3m";
+const string RESET_STYLE = "\033[0m";
+
+const int ASCII_ENTER = 13;
+const int ASCII_BACKSPACE = 8;
+
+// ---- Colores ----
+const string COLOR_FONDO = "\033[48;2;7;112;190m";
+const string TEXTO_BLANCO = "\033[97m";
+
+// ---- Login
+const string LOGIN_USER = "admin";
+const string LOGIN_PASSWORD = "123456";
+
+void mostrarMenu(string texto, vector<string> opciones);
+void imprimirLinea(string texto, string estilos = "", bool saltoLinea = true, int anchoCaja = ANCHO_CAJA);
+string pedirDato(string texto, bool esSecreto = false);
+
+void cargarVistaInicio();
+void cargarVistaLogin();
+void cargarVistaDashboard();
+
+int main()
+{
+	cargarVistaInicio();
+	return 0;
+}
+
+// ------- Vistas --------
+void cargarVistaInicio()
+{
+	system("cls");
+
+	imprimirLinea("          Autos J&J");
+	imprimirLinea("");
+	imprimirLinea("Auditor:", TEXTO_ITALIC, false);
+	imprimirLinea("Mtra. Noemi Torrez Rubio", "", true, 71);
+
+	imprimirLinea("Especialistas:");
+	imprimirLinea("  - Jesus Joan Molina Gutierrez");
+	imprimirLinea("  - Jesus Erick Reyes Sanchez");
+	imprimirLinea("  - Daniel Alberto Noyola Monroy");
+	imprimirLinea("");
+
+	imprimirLinea("Vision de la empresa:");
+	imprimirLinea("Ser la empresa de reparacion de autos siniestrados");
+	imprimirLinea("");
+
+	imprimirLinea("Presiona ENTER para continuar...");
+	imprimirLinea("");
+	cin.get();
+
+	cargarVistaLogin();
+}
+
+void cargarVistaLogin()
+{
+	bool credencialesCorrectas = false;
+	string mensaje = "";
+
+	while (!credencialesCorrectas)
+	{
+		system("cls");
+
+		imprimirLinea("Ingreso al sistema");
+		imprimirLinea("");
+
+		if (!mensaje.empty())
+			imprimirLinea(mensaje, TEXTO_ITALIC);
+
+		string usuario = pedirDato("Ingresa el usuario: ");
+		string password = pedirDato("Ingresa la contrasena: ", true);
+
+		if (usuario != LOGIN_USER || password != LOGIN_PASSWORD)
+			mensaje = "Credenciales incorrectas, intenta de nuevo";
+		else
+			credencialesCorrectas = true;
+	}
+
+	cargarVistaDashboard();
+}
+
+void cargarVistaDashboard()
+{
+	vector<string> opciones = {
+		"Clientes",
+		"Ventas",
+		"Compras",
+		"Proveedores",
+		"Empleados",
+		"Inventario",
+		"Salir"};
+
+	system("cls");
+	imprimirLinea("        Dashboard");
+	mostrarMenu("Opciones disponibles", opciones);
+
+	int opcion = stoi(pedirDato("Ingresa el numero de la opcion: "));
+}
+
+// ----------------------
+void mostrarMenu(string texto, vector<string> opciones)
+{
+	imprimirLinea(texto);
+
+	for (int i = 0; i < opciones.size(); i++)
+	{
+		string opcion = to_string(i + 1) + ". " + opciones[i];
+		imprimirLinea(opcion);
+	}
+}
+
+void imprimirLinea(string texto, string estilos, bool saltoLinea, int anchoCaja)
+{
+	const string sty = TEXTO_BLANCO + COLOR_FONDO + estilos;
+
+	if (saltoLinea)
+		cout << sty << " " << left << setw(anchoCaja) << texto << RESET_STYLE << endl;
+	else
+		cout << sty << " " << texto << RESET_STYLE;
+}
+
+string pedirDato(string texto, bool esSecreto)
+{
+	string dato = "";
+	imprimirLinea(texto, "", false);
+
+	if (!esSecreto)
+	{
+		getline(cin, dato);
+	}
+	else
+	{
+		char caracter;
+		while (true)
+		{
+			caracter = _getch();
+
+			if (caracter == ASCII_ENTER)
+			{
+				cout << endl;
+				break;
+			}
+			else if (caracter == ASCII_BACKSPACE)
+			{
+				if (dato.length() > 0)
+				{
+					dato.pop_back();
+					cout << "\b \b";
+				}
+			}
+			else
+			{
+				dato += caracter;
+				cout << "*";
+			}
+		}
+	}
+	return dato;
+}
